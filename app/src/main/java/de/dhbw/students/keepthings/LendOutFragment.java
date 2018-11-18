ea252a4525723e390f31b058aa7b4ef9a7945462
Benni <benediktlampert19@gmail.com>
2018-11-18 23:33:09 +0100

    #858 comment: Change ListView to RecyclerView in case of better performance and lower need of resources time: 1

--- app/src/main/java/de/dhbw/students/keepthings/LendOutFragment.java
+++ app/src/main/java/de/dhbw/students/keepthings/LendOutFragment.java
@@ -4,19 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LendOutFragment extends Fragment {
 
 
-    private static CustomAdapter adapter;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+
     private ArrayList<LentOutEntry> dataModels;
-    private ListView lentOutEntryList;
     private FloatingActionButton fab;
     private View rootView;
 
@@ -25,30 +30,21 @@ public class LendOutFragment extends Fragment {
                              Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        dataModels = new ArrayList<>();
-
-        dataModels.add(new LentOutEntry("title", "person", "dateTo", "dateFrom"));
-        dataModels.add(new LentOutEntry("Lautsprecher", "Max Mustermann", "30 Oct 18", "9 Oct 18"));
-        dataModels.add(new LentOutEntry("Pfanne", "Max Mustermann", "30 Oct 18", "27 Oct 18"));
-        dataModels.add(new LentOutEntry("30 Euro", "Max Mustermann", "30 Oct 18", "15 Oct 18"));
-        dataModels.add(new LentOutEntry("Fahrradpumpe", "Max Mustermann", "30 Oct 18", "26 Oct 18"));
-        dataModels.add(new LentOutEntry("Fußball", "Max Mustermann", "30 Oct 18", "20 Oct 18"));
-        dataModels.add(new LentOutEntry("Taschenlampe", "Max Mustermann", "30 Oct 18", "6 Oct 18"));
-        dataModels.add(new LentOutEntry("Zelt", "Max Mustermann", "30 Oct 18", "22 Oct 18"));
-        dataModels.add(new LentOutEntry("Kopfhörer", "Max Mustermann", "30 Oct 18", "18 Oct 18"));
-        dataModels.add(new LentOutEntry("Kettensäge", "Max Mustermann", "30 Oct 18", "9 Oct 18"));
-        dataModels.add(new LentOutEntry("Schraubenzieher", "Max Mustermann", "30 Oct 18", "15 Oct 18"));
-        dataModels.add(new LentOutEntry("Biertischgarnitur", "Max Mustermann", "30 Oct 18", "12 Oct 18"));
-        dataModels.add(new LentOutEntry("Anhänger", "Max Mustermann", "30 Oct 18", "5 Oct 18"));
-        dataModels.add(new LentOutEntry("Wanderstiefel", "Max Mustermann", "30 Oct 18", "17 Oct 18"));
-
         rootView = inflater.inflate(R.layout.fragment_lend_out, container, false);
 
-        lentOutEntryList = rootView.findViewById(R.id.lent_out_entry_list);
 
-        adapter = new CustomAdapter(dataModels, rootView.getContext());
 
-        lentOutEntryList.setAdapter(adapter);
+
+        recyclerView = rootView.findViewById(R.id.recycler_view_test);
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+
+        adapter = new RvAdapterKlasse();
+        recyclerView.setAdapter(adapter);
+
+        MainActivity.textView = rootView.findViewById(R.id.item_text_view);
+
 
         fab = rootView.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/de/dhbw/students/keepthings/MainActivity.java
+++ app/src/main/java/de/dhbw/students/keepthings/MainActivity.java
@@ -11,15 +11,54 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
+    static ArrayList<String> itemTitle;
+    static ArrayList<String> itemPerson;
+    static ArrayList<String> itemDateTo;
+    static ArrayList<String> itemDateFrom;
+    static ArrayList<LentOutEntry> lentOutEntrys;
+    static TextView textView;
     private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        lentOutEntrys = new ArrayList<>();
+
+        lentOutEntrys.add(new LentOutEntry("title", "person", "dateTo", "dateFrom"));
+        lentOutEntrys.add(new LentOutEntry("Pfanne", "Max Mustermann", "30 Oct 18", "27 Oct 18"));
+        lentOutEntrys.add(new LentOutEntry("30 Euro", "Max Mustermann", "30 Oct 18", "15 Oct 18"));
+        lentOutEntrys.add(new LentOutEntry("Fahrradpumpe", "Max Mustermann", "30 Oct 18", "26 Oct 18"));
+        lentOutEntrys.add(new LentOutEntry("Fußball", "Max Mustermann", "30 Oct 18", "20 Oct 18"));
+        lentOutEntrys.add(new LentOutEntry("Taschenlampe", "Max Mustermann", "30 Oct 18", "6 Oct 18"));
+        lentOutEntrys.add(new LentOutEntry("Zelt", "Max Mustermann", "30 Oct 18", "22 Oct 18"));
+        lentOutEntrys.add(new LentOutEntry("Kopfhörer", "Max Mustermann", "30 Oct 18", "18 Oct 18"));
+        lentOutEntrys.add(new LentOutEntry("Kettensäge", "Max Mustermann", "30 Oct 18", "9 Oct 18"));
+        lentOutEntrys.add(new LentOutEntry("Schraubenzieher", "Max Mustermann", "30 Oct 18", "15 Oct 18"));
+        lentOutEntrys.add(new LentOutEntry("Biertischgarnitur", "Max Mustermann", "30 Oct 18", "12 Oct 18"));
+        lentOutEntrys.add(new LentOutEntry("Anhänger", "Max Mustermann", "30 Oct 18", "5 Oct 18"));
+        lentOutEntrys.add(new LentOutEntry("Wanderstiefel", "Max Mustermann", "30 Oct 18", "17 Oct 18"));
+        lentOutEntrys.add(new LentOutEntry("Lautsprecher", "Max Mustermann", "30 Oct 18", "9 Oct 18"));
+
+        itemTitle = new ArrayList<>();
+        itemPerson = new ArrayList<>();
+        itemDateTo = new ArrayList<>();
+        itemDateFrom = new ArrayList<>();
+
+        for (LentOutEntry entry : lentOutEntrys) {
+            itemTitle.add(entry.getTitle());
+            itemPerson.add(entry.getPerson());
+            itemDateTo.add(entry.getDateTo());
+            itemDateFrom.add(entry.getDateFrom());
+
+        }
 
 
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -77,8 +116,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 githubIntent.setData(Uri.parse(githubUrl));
                 startActivity(githubIntent);
                 break;
-
-
         }
         drawer.closeDrawer(GravityCompat.START);
         return true;
